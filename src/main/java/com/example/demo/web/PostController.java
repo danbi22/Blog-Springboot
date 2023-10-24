2c86dd6b49569aa88d1b8c873b354afd9ca5d5a0
YUDAHAN <dbekgks98@gmail.com>
2023-10-24 12:16:53 +0900

    post

---
+++ src/main/java/com/example/demo/web/PostController.java
@@ -0,0 +1,15 @@
+package com.example.demo.web;
+
+import groovy.util.logging.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("post")
+public class PostController {
+
+}
