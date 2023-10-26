9c93feb39b34c127b1918562d34d872481b8b3de
YUDAHAN <dbekgks98@gmail.com>
2023-10-26 23:46:32 +0900

    post

--- src/main/java/com/example/demo/web/MemberController.java
+++ src/main/java/com/example/demo/web/MemberController.java
@@ -1,9 +1,14 @@
 package com.example.demo.web;
 
 import groovy.util.logging.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/member")
 public class MemberController {
+
 }
